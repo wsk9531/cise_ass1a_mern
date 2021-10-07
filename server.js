e7ca63f14bf307f532f2e5fde86bb60fc0774307
ben-wsk9531 <wsk9531@autuni.ac.nz>
2021-10-07 16:27:46 +1300

    modified heroku config

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 var cors = require("cors");
+const path = require("path");
 
 // routes
 const router = require("./routes/api/books");
@@ -21,5 +22,12 @@ app.get("/", (req, res) => res.send("Hello World!"));
 app.use("/api/books", router);
 
 const port = process.env.PORT || 8082;
+
+// Build for Heroku
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
